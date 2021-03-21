d2a67c26efdfc3629f9ce709a69e0f7511770bd0
TiLongZS <tilongzs@qq.com>
2021-03-21 12:33:11 +0800

    增加消息输出窗口

--- DialogTest/DialogTestDlg.cpp
+++ DialogTest/DialogTestDlg.cpp
@@ -8,6 +8,8 @@
 #define new DEBUG_NEW
 #endif
 
+#define WMSG_FUNCTION		WM_USER + 1
+
 CDialogTestDlg::CDialogTestDlg(CWnd* pParent /*=nullptr*/)
 	: CDialogEx(IDD_DIALOGTEST_DIALOG, pParent)
 {
@@ -17,11 +19,13 @@ CDialogTestDlg::CDialogTestDlg(CWnd* pParent /*=nullptr*/)
 void CDialogTestDlg::DoDataExchange(CDataExchange* pDX)
 {
 	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDC_EDIT_MSG, _editMsg);
 }
 
 BEGIN_MESSAGE_MAP(CDialogTestDlg, CDialogEx)
 	ON_WM_PAINT()
 	ON_WM_QUERYDRAGICON()
+	ON_MESSAGE(WMSG_FUNCTION, &CDialogTestDlg::OnFunction)
 	ON_BN_CLICKED(IDC_BUTTON1, &CDialogTestDlg::OnBnClickedButton1)
 END_MESSAGE_MAP()
 
@@ -30,10 +34,10 @@ BOOL CDialogTestDlg::OnInitDialog()
 {
 	CDialogEx::OnInitDialog();
 
-
 	SetIcon(m_hIcon, TRUE);
 	SetIcon(m_hIcon, FALSE);
 
+	AppendMsg(L"软件启动");
 
 	return TRUE;
 }
@@ -42,11 +46,10 @@ void CDialogTestDlg::OnPaint()
 {
 	if (IsIconic())
 	{
-		CPaintDC dc(this); // 用于绘制的设备上下文
+		CPaintDC dc(this);
 
 		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
 
-		// 使图标在工作区矩形中居中
 		int cxIcon = GetSystemMetrics(SM_CXICON);
 		int cyIcon = GetSystemMetrics(SM_CYICON);
 		CRect rect;
@@ -54,7 +57,6 @@ void CDialogTestDlg::OnPaint()
 		int x = (rect.Width() - cxIcon + 1) / 2;
 		int y = (rect.Height() - cyIcon + 1) / 2;
 
-		// 绘制图标
 		dc.DrawIcon(x, y, m_hIcon);
 	}
 	else
@@ -63,15 +65,58 @@ void CDialogTestDlg::OnPaint()
 	}
 }
 
-//当用户拖动最小化窗口时系统调用此函数取得光标
-//显示。
 HCURSOR CDialogTestDlg::OnQueryDragIcon()
 {
 	return static_cast<HCURSOR>(m_hIcon);
 }
 
-
 void CDialogTestDlg::OnBnClickedButton1()
 {
 	
 }
+
+void CDialogTestDlg::AppendMsg(const WCHAR* msg)
+{
+	WCHAR* tmpMsg = new WCHAR[wcslen(msg) + 1];
+	memset(tmpMsg, 0, sizeof(WCHAR) * (wcslen(msg) + 1));
+	wsprintf(tmpMsg, msg);
+
+	TheadFunc* pFunc = new TheadFunc;
+	pFunc->Func = ([=]()
+		{
+			if (_editMsg.GetLineCount() > 100)
+			{
+				_editMsg.Clear();
+			}
+
+			CString curMsg;
+			_editMsg.GetWindowTextW(curMsg);
+			curMsg += "\r\n";
+
+			CString strTime;
+			SYSTEMTIME   tSysTime;
+			GetLocalTime(&tSysTime);
+			strTime.Format(L"%02ld:%02ld:%02ld.%03ld ",
+				tSysTime.wHour, tSysTime.wMinute, tSysTime.wSecond, tSysTime.wMilliseconds);
+
+			curMsg += strTime;
+			curMsg += tmpMsg;
+			_editMsg.SetWindowTextW(curMsg);
+			_editMsg.LineScroll(_editMsg.GetLineCount());
+
+			delete[] tmpMsg;
+		});
+
+	PostMessage(WMSG_FUNCTION, (WPARAM)pFunc);
+}
+
+LRESULT CDialogTestDlg::OnFunction(WPARAM wParam, LPARAM lParam)
+{
+	TheadFunc* pFunc = (TheadFunc*)wParam;
+	pFunc->Func();
+	delete pFunc;
+
+	return TRUE;
+}
+
+
--- DialogTest/DialogTestDlg.h
+++ DialogTest/DialogTestDlg.h
@@ -1,35 +1,37 @@
-﻿
-// DialogTestDlg.h: 头文件
-//
+﻿#pragma once
+#include <functional>
+using namespace std;
 
-#pragma once
-
-
-// CDialogTestDlg 对话框
 class CDialogTestDlg : public CDialogEx
 {
-// 构造
 public:
-	CDialogTestDlg(CWnd* pParent = nullptr);	// 标准构造函数
+	CDialogTestDlg(CWnd* pParent = nullptr);
 
-// 对话框数据
 #ifdef AFX_DESIGN_TIME
 	enum { IDD = IDD_DIALOGTEST_DIALOG };
 #endif
 
+	struct TheadFunc
+	{
+		function<void()> Func;
+	};
+
 	protected:
-	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+	virtual void DoDataExchange(CDataExchange* pDX);
 
 
-// 实现
 protected:
 	HICON m_hIcon;
+	CEdit _editMsg;
 
-	// 生成的消息映射函数
 	virtual BOOL OnInitDialog();
 	afx_msg void OnPaint();
 	afx_msg HCURSOR OnQueryDragIcon();
 	DECLARE_MESSAGE_MAP()
+
+
+	void AppendMsg(const WCHAR* msg);
+	LRESULT OnFunction(WPARAM wParam, LPARAM lParam);
 public:
 	afx_msg void OnBnClickedButton1();
 };
